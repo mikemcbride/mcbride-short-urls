99d9d4d7c6a70708589907330f3da4125bba4dc1
Mike McBride <mmcbride1007@icloud.com>
2019-04-02 11:28:36 -0500

    move redirects config into index.js

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const { parse } = require('url')
-const routes = require('./redirects')
+
+const routes = {
+  'mike': 'https://mikemcbride.me',
+  'becky': 'https://putting-on-love.com',
+  'mike-bday': 'https://smile.amazon.com/hz/wishlist/ls/1NKT0C1LMV4P7?ref_=wl_share', 
+  'mike-xmas': 'https://smile.amazon.com/hz/wishlist/ls/1NKT0C1LMV4P7?ref_=wl_share',
+  'becky-bday': 'https://smile.amazon.com/hz/wishlist/ls/2C16Q3JHY6J00?ref_=wl_share',
+  'becky-xmas': 'https://smile.amazon.com/hz/wishlist/ls/2C16Q3JHY6J00?ref_=wl_share',
+  'rose-bday': 'https://smile.amazon.com/hz/wishlist/ls/MAWRXNA3K831?ref_=wl_share',
+  'rose-xmas': 'https://smile.amazon.com/hz/wishlist/ls/MAWRXNA3K831?ref_=wl_share',
+  'kolbe-bday': 'https://smile.amazon.com/hz/wishlist/ls/3D4RIPZ1P14C8?ref_=wl_share',
+  'kolbe-xmas': 'https://smile.amazon.com/hz/wishlist/ls/3D4RIPZ1P14C8?ref_=wl_share',
+  'leo-bday': 'https://smile.amazon.com/baby-reg/michael-mcbride-becky-mcbride-november-2018-ballwin/1IPTVKO44O453',
+  'leo-xmas': 'https://smile.amazon.com/baby-reg/michael-mcbride-becky-mcbride-november-2018-ballwin/1IPTVKO44O453',
+}
 
 module.exports = (req, res) => {
   const { pathname } = parse(req.url, true)
-  const route = pathname.toLowerCase().trim().replace('/', '')
-  let location = routes[route] || 'https://github.com/mikemcbride/mcbride-short-urls'
+  const path = pathname.toLowerCase().trim().replace('/', '')
+  let location = routes[path] || 'https://github.com/mikemcbride/mcbride-short-urls'
   res.writeHead(302, { 'Location': location })
   res.end()
 }
