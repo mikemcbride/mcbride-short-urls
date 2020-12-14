a10cd1b414b8d6a59cceb028f752ec4887ade016
Mike McBride <mike.mcbride@hey.com>
2020-12-14 00:43:30 -0600

    add xmas redirect

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const routes = require('./routes.json')
 module.exports = (req, res) => {
   const { pathname } = parse(req.url, true)
   const path = pathname.toLowerCase().trim().replace('/', '')
-  
+
   if (routes[path] !== undefined) {
     res.writeHead(302, { 'Location': routes[path] })
   } else {
@@ -12,6 +12,6 @@ module.exports = (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' })
     res.write(html)
   }
-  
+
   res.end()
 }
